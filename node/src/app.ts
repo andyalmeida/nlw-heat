22cc6e839ac2be571e5909e3d441753543cec340
André Almeida <andalvalmeida@gmail.com>
2021-10-18 21:18:00 -0300

    Create GitHub login route

--- node/src/app.ts
+++ node/src/app.ts
@@ -3,6 +3,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/github', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
 app.listen(4000, () => {
   console.log('🚀 Server is running at http://localhost:4000');
 });
