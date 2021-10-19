8cd82e2f4ed174e85e9be1f993e99f2c1d7e52c3
André Almeida <andalvalmeida@gmail.com>
2021-10-19 11:47:06 -0300

    Create GetLastMessages controller

--- node/src/routes.ts
+++ node/src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
+import { GetLastMessagesController } from './controllers/GetLastMessagesController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
@@ -8,5 +9,7 @@ const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle);
 
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
+
+router.get('/messages/last', new GetLastMessagesController().handle);
 
 export { router }
