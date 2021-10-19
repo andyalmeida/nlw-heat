4ccceb858f0622810bdaca761b375c93d25a561f
André Almeida <andalvalmeida@gmail.com>
2021-10-18 22:18:00 -0300

    Get user info from GitHub

--- node/src/services/AuthenticateUserService.ts
+++ node/src/services/AuthenticateUserService.ts
@@ -1,10 +1,21 @@
 import axios from "axios";
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  login: string;
+  id: number;
+  avatar_url: string;
+  name: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url, null, {
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
       params: {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -13,6 +24,12 @@ class AuthenticateUserService {
       headers: {
         "Accept": "application/json",
       },
+    });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${accessTokenResponse.access_token}`,
+      }
     });
 
     return response.data;
