20799bae213410b598dc42f5ba08a7d263c9c6df
André Almeida <andalvalmeida@gmail.com>
2021-10-19 12:02:50 -0300

    Create UserProfile controller

--- node/src/routes.ts
+++ node/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLastMessagesController } from './controllers/GetLastMessagesController';
+import { UserProfileController } from './controllers/UserProfileController';
 import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router();
@@ -11,5 +12,7 @@ router.post('/authenticate', new AuthenticateUserController().handle);
 router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 
 router.get('/messages/last', new GetLastMessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new UserProfileController().handle);
 
 export { router }
