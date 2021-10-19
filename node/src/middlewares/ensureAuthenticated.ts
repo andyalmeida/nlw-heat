0bd38e72ec1ac32c4fc20109de28ddc39ebdd213
André Almeida <andalvalmeida@gmail.com>
2021-10-19 09:43:36 -0300

    Create middleware to validate authentication

---
+++ node/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+  sub: string;
+}
+
+export function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction) {
+
+  const authToken = req.headers.authorization;
+  if (!authToken) {
+    return res.status(401).json({ errorCode: 'token.missing' });
+  }
+
+  const [, token] = authToken.split(' ');
+
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+    req.user_id = sub;
+    return next();
+  } catch {
+    return res.status(401).json({ errorCode: 'token.invalid' });
+  }
+}
