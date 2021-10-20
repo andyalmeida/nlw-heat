8d27af975c234f9f0b692dc00e90a4298cc2a6f6
André Almeida <andalvalmeida@gmail.com>
2021-10-19 21:42:31 -0300

    Create MessageList component

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,11 @@
 import style from './App.module.scss';
 import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
 
 export function App() {
   return (
     <main className={style.contentWrapper}>
+      <MessageList />
       <LoginBox />
     </main>
   )
