b5c76d657f4f79a662ef81cf7c7bcc42929716db
André Almeida <andalvalmeida@gmail.com>
2021-10-21 13:28:47 -0300

    Alter status bar style

--- react-native/src/screens/home/styles.ts
+++ react-native/src/screens/home/styles.ts
@@ -1,7 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+
+import { COLORS } from '../../theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: COLORS.BLACK_SECONDARY
   }
 });
