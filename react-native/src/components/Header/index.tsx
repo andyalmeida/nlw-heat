51165694d819ce537a522127cdd76e7d9465ac1e
André Almeida <andalvalmeida@gmail.com>
2021-10-21 20:45:52 -0300

    Create UserPhoto component

--- react-native/src/components/Header/index.tsx
+++ react-native/src/components/Header/index.tsx
@@ -4,14 +4,19 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 
 import LogoSvg from '../../assets/logo.svg';
+import { UserPhoto } from '../UserPhoto';
 
 export function Header() {
   return (
     <View style={styles.container}>
       <LogoSvg />
-      <TouchableOpacity>
-        <Text style={styles.logoutText}>Sair</Text>
-      </TouchableOpacity>
+
+      <View style={styles.logoutButton}>
+        <TouchableOpacity>
+          <Text style={styles.logoutText}>Sair</Text>
+        </TouchableOpacity>
+        <UserPhoto imageUri='https://github.com/andyalmeida.png' />
+      </View>
     </View>
   );
 }
