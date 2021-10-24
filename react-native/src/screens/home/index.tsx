6ac7c42a90db7078e39bedf367cb9c7e3d8fe8eb
André Almeida <andalvalmeida@gmail.com>
2021-10-24 06:25:01 -0300

    Create Button component

--- react-native/src/screens/home/index.tsx
+++ react-native/src/screens/home/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { COLORS } from '../../theme';
 
 import { styles } from './styles';
 
@@ -10,6 +12,12 @@ export function Home() {
     <View style={styles.container}>
       <Header />
       <MessageList />
+      <Button
+        title="ENTRAR COM O GITHUB"
+        color={COLORS.BLACK_PRIMARY}
+        backgroundColor={COLORS.YELLOW}
+        icon="github"
+      />
     </View>
   );
 }
